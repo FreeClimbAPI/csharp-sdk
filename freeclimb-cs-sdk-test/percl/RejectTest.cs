2022953f8091ff3159e9c7fc4b8a4e8b2606d340
Francis San Filippo <fsanfilipp@vailsys.com>
2019-12-02 16:29:53 -0600

    create reject PerCL feature

---
+++ freeclimb-cs-sdk-test/percl/RejectTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.freeclimb.percl;
+
+namespace freeclimb_cs_sdk_test.percl
+{
+    [TestClass]
+
+    public class RejectTest
+    {
+        [TestMethod]
+        public void DefaultRejectCommandToJsonTest()
+        {
+            Reject reject = new Reject();
+
+            string json = reject.toJson();
+
+            Assert.IsNotNull(json);
+            Assert.AreEqual(json, "{\"Reject\":{}");
+        }
+
+        [TestMethod]
+        public void RejectCommandToJsonTest()
+        {
+            Reject reject = new Reject();
+            reject.setReason("not available right now");
+
+            string json = reject.toJson();
+
+            Assert.IsNotNull(json);
+            Assert.AreEqual(json, "{\"Reject\":{\"reason\":\"not available right now\",}}");
+        }
+    }
+}
---
+++ freeclimb-cs-sdk/percl/Reject.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using com.freeclimb.api;
+
+namespace com.freeclimb.percl
+{
+    /// <summary>
+    /// Reject PerCL command.
+    /// </summary>
+    [JsonObject]
+    public sealed class Reject : PerCLCommand
+    {
+        [JsonProperty(PropertyName = "reason")]
+        private string reason = null;
+
+        /// <summary>
+	    /// Helper method to build a JSON string from a Reject instance.
+        /// </summary>
+        /// <returns>A JSON string equivalent to the Reject instance.</returns>
+        /// <exception cref="FreeClimbJSONException">Thrown upon deserialize failure.</exception>
+        public override string toJson()
+        {
+            try
+            {
+                JsonSerializer jsonSerializer = JsonSerializer.Create();
+                jsonSerializer.NullValueHandling = NullValueHandling.Ignore;
+
+                StringBuilder strb = new StringBuilder();
+                jsonSerializer.Serialize(new StringWriter(strb), toKvp());
+
+                return strb.ToString();
+            }
+            catch (Exception e)
+            {
+                throw new FreeClimbJSONException(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Retrieve the reason value.
+        /// </summary>
+        /// <returns>The text value of the object.</returns>
+        public string getReason { get { return this.reason; } }
+
+        /// <summary>
+        /// Set the reason value.
+        /// </summary>
+        /// <param name="val">reason value.</param>
+        public void setReason(string val) { this.reason = val; }
+
+
+        public override IDictionary<string, object> toKvp()
+        {
+            // change all properties with settings to a dictionary
+            IDictionary<string, object> props = new Dictionary<string, object>();
+
+            props.Add("reason", getReason);
+
+            IDictionary<string, object> command = new Dictionary<string, object>();
+            command.Add("Reject", props);
+
+            return command;
+        }
+    }
+}
